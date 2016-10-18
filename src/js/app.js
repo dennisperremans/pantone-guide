a82f31c0c1f19bf146c48ebf5c5f7291e8deef80
Perremans <DennisPer@youforce.net>
2016-10-18 14:01:15 +0200

    init

---
+++ src/js/app.js
@@ -0,0 +1,40 @@
+$(function() {
+
+	// set text color
+	function isDark( color ) {
+    	var match = /rgb\((\d+).*?(\d+).*?(\d+)\)/.exec(color);
+	    return ( match[1] & 255 )
+	         + ( match[2] & 255 )
+	         + ( match[3] & 255 )
+	           < 3 * 256 / 2;
+	}
+
+	$('.swatch').each(function() {
+    	$(this).css("color", isDark($(this).css("background-color")) ? 'white' : 'black');
+	});
+
+
+	// Search color
+	$(".search").keyup(function(){
+ 
+        // Retrieve the input field text and reset the count to zero
+        var filter = $(this).val(), count = 0;
+ 
+        // Loop through the comment list
+        $(".swatch").each(function(){
+ 
+            // If the list item does not contain the text phrase fade it out
+            if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+                $(this).fadeOut();
+ 
+            // Show the list item if the phrase matches and increase the count by 1
+            } else {
+                $(this).show();
+            }
+        });
+    });
+
+
+});
+
+
